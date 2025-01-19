da52c663bd91c10af066f2036f56d26818d1d811
cadi <dbtmdgns4296@naver.com>
2025-01-20 01:02:42 +0900

    feat: [UI] Main Navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import ContentBox from "@components/ContentBox";
 import Intro from "@contents/intro";
+import MainNavigation from "@contents/navigation/mainNavigation";
 import { Stack } from "@mui/material";
 import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <main>
+      <MainNavigation/>
       <Stack
         direction="row"
         spacing={2}
